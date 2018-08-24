62c3c4cd9bfaeccf9c5a149d81d632f3f0c6de6d
jarome <ja_rome@163.com>
2018-08-25 00:01:22 +0800

    增加左边树

--- src/views/client/index.vue
+++ src/views/client/index.vue
@@ -1,9 +1,14 @@
-<style>
+<style type="scss">
     .leftMenu {
         background-color: #D3DCE6;
         color: #333;
         text-align: center;
         line-height: 200px;
+        width: 200px;
+    }
+
+    .leftList{
+        height: 100%;
     }
 
     .clientMain {
@@ -16,10 +21,61 @@
 </style>
 <template>
     <el-container>
-        <el-aside width="200px" class="leftMenu">Aside</el-aside>
+        <el-aside class="leftMenu">
+            <el-tree :data="data" class="leftList" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+        </el-aside>
         <el-main class="clientMain">Main</el-main>
     </el-container>
 </template>
 <script>
-    export default {}
+    export default {
+        data(){
+            return {
+                data: [{
+                    label: '一级 1',
+                    children: [{
+                        label: '二级 1-1',
+                        children: [{
+                            label: '三级 1-1-1'
+                        }]
+                    }]
+                }, {
+                    label: '一级 2',
+                    children: [{
+                        label: '二级 2-1',
+                        children: [{
+                            label: '三级 2-1-1'
+                        }]
+                    }, {
+                        label: '二级 2-2',
+                        children: [{
+                            label: '三级 2-2-1'
+                        }]
+                    }]
+                }, {
+                    label: '一级 3',
+                    children: [{
+                        label: '二级 3-1',
+                        children: [{
+                            label: '三级 3-1-1'
+                        }]
+                    }, {
+                        label: '二级 3-2',
+                        children: [{
+                            label: '三级 3-2-1'
+                        }]
+                    }]
+                }],
+                defaultProps: {
+                    children: 'children',
+                    label: 'label'
+                }
+            };
+        },
+        methods: {
+            handleNodeClick(data) {
+                console.log(data);
+            }
+        }
+    };
 </script>
